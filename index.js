a2634b443c31555ef240bd2d7883b4436a651027
Mohsen Azimi <mazimi@apigee.com>
2014-08-15 14:25:59 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+    restful = require('node-restful'),
+    mongoose = restful.mongoose;
+    secrets = require('./secrets')
+    bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(express.query());
+
+var mongooseUrl = 'mongodb://' + secrets.mongolab.petstore.username + ':' +
+  secrets.mongolab.petstore.password + '@ds055709.mongolab.com:55709/petstore';
+mongoose.connect(mongooseUrl);
+
+var Pet = app.pet = restful.model('pet', mongoose.Schema({
+    name: 'string',
+    birthday: 'number',
+  }))
+  .methods(['get', 'post', 'put', 'delete']);
+
+Pet.register(app, '/pet');
+
+app.listen(3000);
---
+++ secrets.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongolab: {
+    petstore: {
+      username: 'admin',
+      password: 'admin'
+    }
+  }
+};
